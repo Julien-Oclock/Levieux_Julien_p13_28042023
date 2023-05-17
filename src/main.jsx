4b088759ccb8e194ab509f1d6fb738c68616ea89
Julien-Drotek <julien.levieux@drotek.com>
2023-05-17 15:51:21 +0200

    install redux + init store files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.scss'
+
+// redux store
+import { Provider } from 'react-redux'
+import {store} from './Store/Store.js' 
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
