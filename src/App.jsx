dbf0a06e19bc67ec7604e0becf53a04cb8108cc0
Julien-Drotek <julien.levieux@drotek.com>
2023-05-30 13:33:37 +0200

    fix: rename import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import HomePage from './pages/HomePage'
-import SignIn from './pages/SignIn'
-import User from './pages/User'
+import HomePage from './Pages/HomePage'
+import SignIn from './Pages/SignIn'
+import User from './Pages/User'
 
 import './styles.scss'
 
