f157c3b74a199a2053947625b103be4830f3fdd5
Julien Levieux <julien.levieux@drotek.com>
2023-06-25 16:03:14 +0200

    Add cookies service

---
+++ src/app/Service/cookiesServices.js
@@ -0,0 +1,31 @@
+
+// Source: https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+export function GetCookie(name) {
+    let cookie = {};
+    document.cookie.split(';').forEach(function (el) {
+        let [key, value] = el.split('=');
+        cookie[key.trim()] = value;
+    })
+    return cookie[name.toUpperCase()] ?
+        JSON.parse(cookie[name.toUpperCase()]) :
+        null;
+}
+
+/**
+ * Set / Update Cookie by name
+ * @param cookieName 
+ * @param body 
+ */
+export function SetCookie(name, body) {
+    document.cookie = `${name.toUpperCase()}=${JSON.stringify(body)};SameSite=Strict;`
+}
+
+/**
+ * Remove cookie by name
+ * @param cookieName 
+ */
+export function RemoveCookie(name) {
+    if (GetCookie(name)) {
+        document.cookie = name.toUpperCase() + '=;Max-Age=-99999999;'
+    }
+}
